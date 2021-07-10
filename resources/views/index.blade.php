8596de0681cd18314bd99c99457cc69190b33238
Mobashir Monim <mobashirmonim@gmail.com>
2021-07-10 20:05:55 +0600

    Update views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello</title>
-</head>
-<body>
-    <h1>Hello World!!</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 hero-area d-flex" id="hero-area">
+            <h1 class="my-auto text-center text-white"><b>CSE470</b> <span class="text-secondary">Tutorial Blog</span></h1>
+            <a href="#" class="btn btn-dark action-btn btn-add"><i class="fas fa-plus"></i></a>
+            <a href="#" class="btn btn-dark action-btn btn-view"><i class="fas fa-eye"></i></a>
+        </div>
+    </div>
+@endsection
