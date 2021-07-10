4e5c2f8200ee20a85c7fd5de6fdedda0fb276e1a
Mobashir Monim <mobashirmonim@gmail.com>
2021-07-10 20:55:02 +0600

    Update blog create page

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -4,33 +4,25 @@
     <form action="/store" method="POST">
         @csrf
         <div class="row">
-            <div class="col-md-12 my-3">
-                <input type="text" name="title" class="form-control" placeholder="Blog Title">
+            <div class="col-md-12 mt-3">
+                <div class="input-group blog-input-group">
+                    <input type="text" name="title" class="form-control blog-input-group w-75" placeholder="Blog Title">
+                    <select name="category" class="form-control blog-input-group">
+                        <option value="">Select a category</option>
+                        <option value="Category 1">Category 1</option>
+                        <option value="Category 2">Category 2</option>
+                        <option value="Category 3">Category 3</option>
+                        <option value="Category 4">Category 4</option>
+                        <option value="Category 5">Category 5</option>
+                    </select>
+                </div>
             </div>
         </div>
     
         <div class="row">
-            <div class="col-md-12 my-3">
-                <textarea name="body" class="form-control" cols="30" rows="10" placeholder="Type the contents of the blog here"></textarea>
-            </div>
-        </div>
-    
-        <div class="row">
-            <div class="col-md-12 my-3">
-                <select name="category" class="form-control">
-                    <option value="">Select a category</option>
-                    <option value="Category 1">Category 1</option>
-                    <option value="Category 2">Category 2</option>
-                    <option value="Category 3">Category 3</option>
-                    <option value="Category 4">Category 4</option>
-                    <option value="Category 5">Category 5</option>
-                </select>
-            </div>
-        </div>
-    
-        <div class="row">
-            <div class="col-md-12 my-3">
-                <button class="btn btn-dark w-100">Save</button>
+            <div class="col-md-12">
+                <textarea name="body" class="form-control blog-body" cols="30" rows="10" placeholder="Type the contents of the blog here"></textarea>
+                <button class="btn btn-dark w-100 btn-save"><i class="fas fa-save fa-3x"></i></button>
             </div>
         </div>
     </form>
