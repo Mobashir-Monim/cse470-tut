208263cfad4df021e4a6e3e73b806cbfd0c90b79
Mobashir Monim <mobashirmonim@gmail.com>
2021-07-10 15:34:34 +0600

    Create controller and view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\IndexController::class, 'index']);
+Route::get('/test', [App\Http\Controllers\IndexController::class, 'test']);
